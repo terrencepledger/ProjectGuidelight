acb01271f9a4e6b3ab28877d6f2a22e946821f0d
Terrence <terrencepledger@gmail.com>
2026-02-10 10:57:56 -0600

    Cleanup temp files

---
+++ temp-clean.bat
@@ -0,0 +1,4 @@
+cd /d "C:\Users\Terrence\Desktop\Desktop - Work\GPT\Guidelight"
+git add -A
+git commit -m "Cleanup temp files"
+git push origin main
--- temp-release.bat
+++
@@ -1,5 +0,0 @@
-cd /d "C:\Users\Terrence\Desktop\Desktop - Work\GPT\Guidelight"
-git add -A
-git commit -m "v1.3.0: Always-on red letter Bible for KJV and WEB"
-git tag v1.3.0
-git push origin main --tags
